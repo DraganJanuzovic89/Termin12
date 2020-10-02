c76699093b9a9251386537bb323846387c68658a
Dragan Januzovic <d.januzovic@hotmail.com>
2020-10-02 21:24:23 +0200

    Initial commit

---
+++ app/src/main/java/com/ftninformatika/termin12/DrugaAktivnost.java
@@ -0,0 +1,14 @@
+package com.ftninformatika.termin12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrugaAktivnost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_druga_aktivnost);
+    }
+}
